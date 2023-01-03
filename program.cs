af075737931bf451ab1cfdb1b529491d3d15d5db
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-03 09:58:44 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 123;
+            int y = 321;
+            int z = x + y;
+            int age = 21;
+
+            Console.WriteLine(x + y);
+            Console.WriteLine("your age is" + age);
+
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
